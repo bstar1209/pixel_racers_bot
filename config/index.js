14e81566d3ecd2f6eea346d69088fa85bae357d7
bstar <lakal0809@outlook.com>
2022-03-30 15:43:03 +0800

    added new feature

--- config/index.js
+++ config/index.js
@@ -1,6 +1,9 @@
 export const COMMAND_PREFIX = '?';
 
-export const DISCORD_TOKEN = ''
+export const PIXEL_RACERS_CARS_DISCORD_TOKEN = ''
+export const PIXEL_RACERS_ENGINES_DISCORD_TOKEN = ''
+export const PIXEL_RACERS_PIT_CREW_DISCORD_TOKEN = ''
+export const PIXEL_RACERS_HONORARIES_DISCORD_TOKEN= ''
 
 export const NETWORK = 'mainnet-beta';
 
@@ -10,3 +13,4 @@ export const CHANNEL_ID = ""
 export const PIXEL_RACERS_CARS_ID = 'Hvq6K3BvaBZFQyYYoc3gxwh4ahFWvbLVuTp6UWgfXfMT'
 export const PIXEL_RACERS_ENGINES_ID = 'PXRzehgVwbtzoUoB4p2rw2g2LvH8NbmxakdTMjg13Cm'
 export const PIXEL_RACERS_PIT_CREW_ID = 'DrjSrcb1sxQwq3FUs1Me83o8vCgNK3TeRYZb9DQmkU7Q'
+export const PIXEL_RACERS_HONORARIES_ID = 'B6uRseKbKcahj5fWGc2gK2z8rv3Hj6oDDQukervvynjN'
